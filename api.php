5d55bda0e759fa65e7569456741df83e7cd358c8
afzafri <zafri_21596@yahoo.com>
2017-05-13 22:31:02 +0800

    Fetch webpage content from yabla site

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_GET['define']))
+{
+	$define = $_GET['define']; 
+    $url = "https://chinese.yabla.com/chinese-english-pinyin-dictionary.php?define=".$define; # yabla website url
+   
+    $ch = curl_init(); # initialize curl object
+    curl_setopt($ch, CURLOPT_URL, $url); # set url
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); # disable SSL verification (THIS IS NOT PROPER/SAFE)
+    $result = curl_exec($ch); # execute curl, fetch webpage content
+    $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # receive http response status
+    $err = curl_error($ch);
+    curl_close($ch);  # close curl
+    
+    print_r($result); # test print scraped page
+}
+
+?>
