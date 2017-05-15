7960f82f232a1dedfa9ddb28b8251c376ba30439
afzafri <zafri_21596@yahoo.com>
2017-05-15 22:45:12 +0800

    check if search result found or not, if found, parsed data

--- api.php
+++ api.php
@@ -34,44 +34,55 @@ if(isset($_GET['define']))
     
     $jsondict['http_code'] = $httpstatus; # set http response code into the array
     
-    for($i=0;$i<count($parsed[0]);$i++)
+    # check if search result found or not
+    if(strpos($parsed[0][0], 'No matches found for') !== false) 
     {
+        $jsondict['message'] = "No matches found for '".$define."'";
+    }
+    else
+    {
+        # data found, start parsing data
+        $jsondict['message'] = "Matches found for '".$define."'";
         
-        # --------- PARSE TO GET CHINESE CHARACTERS ----------
-        $paternword = '#<span class="word">([\w\W]*?)</span>#';
-        preg_match_all($paternword, $parsed[0][$i], $word);
-        
-        $paternword2 = '#<a([\w\W]*?)</a>#';
-        preg_match_all($paternword2, implode('',$word[0]), $cnword);
-        
-        $combword = '';
-        
-        for($j=0;$j<count($cnword[0]);$j++)
+        for($i=0;$i<count($parsed[0]);$i++)
         {
-            $combword .= strip_tags($cnword[0][$j]);
+
+            # --------- PARSE TO GET CHINESE CHARACTERS ----------
+            $paternword = '#<span class="word">([\w\W]*?)</span>#';
+            preg_match_all($paternword, $parsed[0][$i], $word);
+
+            $paternword2 = '#<a([\w\W]*?)</a>#';
+            preg_match_all($paternword2, implode('',$word[0]), $cnword);
+
+            $combword = '';
+
+            for($j=0;$j<count($cnword[0]);$j++)
+            {
+                $combword .= strip_tags($cnword[0][$j]);
+            }
+
+            $jsondict['data'][$i]['cn_char'] = $combword;
+
+
+            # --------- PARSE TO GET PINYIN ----------
+
+            $paternpinyin = '#<span class="pinyin">([\w\W]*?)</span>#';
+            preg_match_all($paternpinyin, $parsed[0][$i], $pinyin);
+
+            $jsondict['data'][$i]['pinyin'] = strip_tags(implode('',$pinyin[0]));
+
+
+            # --------- PARSE TO GET MEANINGS ----------
+
+            $paternmeaning = '#<div class="meaning">([\w\W]*?)</div>#';
+            preg_match_all($paternmeaning, $parsed[0][$i], $meanings);
+
+            for($j=0;$j<count($meanings[0]);$j++)
+            {
+                $jsondict['data'][$i]['meanings'] = preg_split("/\\r\\n|\\r|\\n/", strip_tags($meanings[0][$j]));
+            }
+
         }
-        
-        $jsondict['data'][$i]['cn_char'] = $combword;
-        
-        
-        # --------- PARSE TO GET PINYIN ----------
-        
-        $paternpinyin = '#<span class="pinyin">([\w\W]*?)</span>#';
-        preg_match_all($paternpinyin, $parsed[0][$i], $pinyin);
-        
-        $jsondict['data'][$i]['pinyin'] = strip_tags(implode('',$pinyin[0]));
-        
-        
-        # --------- PARSE TO GET MEANINGS ----------
-        
-        $paternmeaning = '#<div class="meaning">([\w\W]*?)</div>#';
-        preg_match_all($paternmeaning, $parsed[0][$i], $meanings);
-        
-        for($j=0;$j<count($meanings[0]);$j++)
-        {
-            $jsondict['data'][$i]['meanings'] = preg_split("/\\r\\n|\\r|\\n/", strip_tags($meanings[0][$j]));
-        }
-        
     }
     
     # project info
